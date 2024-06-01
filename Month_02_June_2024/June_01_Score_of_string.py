f28e0d2d4cfd5bfa9c1623e6655bf759193a0b55
sbibek51 <sktbibek51@gmail.com>
2024-06-01 12:34:16 -0400

    june 01 score of a string

---
+++ Month_02_June_2024/June_01_Score_of_string.py
@@ -0,0 +1,45 @@
+#Bibek Shiwakoti
+'''
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+
+
+
+Example 1:
+
+Input: s = "hello"
+
+Output: 13
+
+Explanation:
+
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+
+Input: s = "zaz"
+
+Output: 50
+
+Explanation:
+
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+'''
+
+#Note: ord() gives ASCII value in python
+
+class Solution(object):
+    def scoreOfString(self, s):
+        sum = 0
+        for i in range(len(s)-1):
+            diff =(ord(s[i])-ord(s[i+1]))
+            if diff < 0:
+                diff = diff*-1
+            sum = sum +diff
+        return sum
+
+
+obj = Solution()
+print(obj.scoreOfString('hello'))
