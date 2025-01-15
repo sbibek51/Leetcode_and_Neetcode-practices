fab2a36f59be5884a90bc7fe12e22ec187780cb0
sbibek51 <sktbibek51@gmail.com>
2025-01-15 17:40:48 -0500

    stack valid parenthes

---
+++ Neetcode_150/stack/valid_parenthesis.py
@@ -0,0 +1,45 @@
+# 1/15/2025
+# Bibek Shiwakoti
+# Valid parenthes
+
+'''
+Valid Parentheses
+Solved
+You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+'''
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")":"(","]":"[","}":"{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+
+obj = Solution()
+
+def call_me(s):
+    if(obj.isValid(s)):
+        print('given parenthes',s, 'is valid')
+    else:
+        print('given parenthes',s, 'is not valid')
+
+s = "([{}])"
+call_me(s)
+
+s = "([{}])}"
+call_me(s)
