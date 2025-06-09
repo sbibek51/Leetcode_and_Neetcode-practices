bee92ccbf11362d19ac35d74191ac023c41808e4
sbibek51 <sktbibek51@gmail.com>
2025-06-09 18:55:03 -0400

    basic notes folder to quick review upon need added

---
+++ Neetcode_150/Fundamentals and notes/enumerate.py
@@ -0,0 +1,11 @@
+#June 9 2025
+# notes on enemurate
+
+# Enumerate() is a built in function which helps to iterate both index and the element
+# By default, the index is the first element of the tuple, and the value is the second.
+
+# example use
+colors = ['red','green','yellow','white']
+
+for index,color in enumerate(colors):
+    print(index,color)
